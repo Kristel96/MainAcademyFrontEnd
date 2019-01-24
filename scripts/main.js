50a14ea286b07dbe97b258945e6f74248e82999f
Khrystyna Rymar <khrystyna.rymr@gmail.com>
2019-01-24 18:50:00 +0200

    location added

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,22 @@
-var newTab;
-function openTab(){
-    newTab =  window.open("2e2w", "_blank", "width=300, height=300, scrollbars=yes ");
-}
-function resizeTab() {
-    //newTab.resizeTo(500,500);
-    newTab.resizeBy(300,300);
-    newTab.focus();
-}
-function closeTab() {
-    newTab.close();
-}
-function scroller(){
-    newTab.scrollBy(20,1000);
-    newTab.focus();
-}
+var brown = document.getElementsByClassName("brown");
 
-
-function hrefDetails() {
-    var loc = window.location;
-
-    var a = document.getElementById("1");
-    a.innerText = "Host: " + loc.host;
-    a.style.fontStyle="italic";
-    var b = document.getElementById("2");
-    b.innerText = "Hostname: " + loc.hostname;
-    var c =  document.getElementById("3");
-    c.innerText = "Origin: " + loc.origin;
-    var d = document.getElementById("4");
-    d.innerText = "Pathname: " + loc.pathname;
-    var e = document.getElementById("5");
-    e.innerText = "Port: " + loc.port;
-    var f = document.getElementById("6");
-    f.innerText = "Protocol: " + loc.protocol;
-
+for(var i=0; i<brown.length; i++) {
+    let element = brown[i];
+    element.innerHTML = "brown";
+    let estyle = element.style;
+       if(i%2===0){
+        estyle.backgroundColor = "red";
+    }else {
+        estyle.backgroundColor = "green";
+    }
+    if(i == brown.length-1){
+        estyle.backgroundColor = "violet";
+    }
+    if((i+1)%3===0){
+        estyle.backgroundColor = "yellow";
+    }
+    estyle.border = "1px solid white";
+    estyle.height = "40px";
+    estyle.color = "white";
 }
-
 
