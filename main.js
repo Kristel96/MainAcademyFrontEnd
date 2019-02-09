c4be94a55a75f6e5b17197208ba4e3e81cb2de6e
Khrystyna Rymar <khrystyna.rymr@gmail.com>
2019-02-09 11:23:15 +0200

    added local storage

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-// var a = document.getElementById("hek");
-// console.log(a.value);
+var myCar = {
+    wheels: 4,
+    doors: 4,
+    engine: 1,
+    name: "Jaguar"
+}
+var globalKey = 'carInLocalStorage';
+var localCar = stringfyObj(myCar);
+localStorage.setItem(globalKey, localCar);
+function rewriteCar (){
+    myCar.name =  "Audi";
+    myCar.engine = 2;
+    myCar.helloWorls = "";
+    localCar = stringfyObj(myCar);
+    localStorage.setItem(globalKey, localCar);
+}
+
+function stringfyObj(objToStringfy) {
+    return JSON.stringify(objToStringfy);
+}
+function globalRemove(){
+    removeCar(globalKey);
+}
+function removeCar(key){
+  localStorage.removeItem(key);
+}
