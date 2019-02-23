491a572f3270f5229f880ca67390e89aaa1e6855
Khrystyna Rymar <khrystyna.rymr@gmail.com>
2019-02-23 08:40:18 +0200

    added reqres api

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,40 @@ $( document ).ready(function() {
      // GET ALL USERS
     $(".page").click(function (){
     var currentPage = this.innerHTML;
-        getUsers (currentPage);
+        getColors();
     });
-    function getUsers(page){
+    // function getUsers(page){
+    //     $.ajax({
+    //         url: "https://reqres.in/api/users?delay="+page,
+    //         type: "GET",
+    //         success: function(users){
+    //             $("#main-container").empty();
+    //             var currentUsers = users.data;
+    //             for(var i=0; i<currentUsers.length; i++){
+    //                 $("#main-container").append("<div class=\"card\">\n" +
+    //                     "    <img src=\""+currentUsers[i].avatar+"\" class=\"card-img-top\">\n" +
+    //                     "    <div class=\"card-body\">\n" +
+    //                     "        <p class=\"card-text\">"+currentUsers[i].first_name+"</p>\n" +
+    //                     "        <p class=\"card-text\">"+currentUsers[i].last_name+"</p>\n" +
+    //                     "    </div>\n" +
+    //                     "    </div>")
+    //             }
+    //
+    //         }
+    //     });
+    // }
+    function getColors(){
         $.ajax({
-            url: "https://reqres.in/api/users?page="+page,
+            url: "https://reqres.in/api/unknown",
             type: "GET",
             success: function(users){
                 $("#main-container").empty();
                 var currentUsers = users.data;
                 for(var i=0; i<currentUsers.length; i++){
-                    $("#main-container").append("<div class=\"card\">\n" +
-                        "    <img src=\""+currentUsers[i].avatar+"\" class=\"card-img-top\">\n" +
-                        "    <div class=\"card-body\">\n" +
-                        "        <p class=\"card-text\">"+currentUsers[i].first_name+"</p>\n" +
-                        "        <p class=\"card-text\">"+currentUsers[i].last_name+"</p>\n" +
-                        "    </div>\n" +
-                        "    </div>")
+                    $("#main-container").append("<div class='cont' style='background-color:"+currentUsers[i].color+"'></div>" +
+                        "<div class='cont'>"+currentUsers[i].name+"</div>" +
+                        "<div class='cont'>"+currentUsers[i].pantone_value+"</div>" +
+                        "<div class='cont'>"+currentUsers[i].year+"</div>" )
                 }
 
             }
